c0abd7cac009d51e10bb881003dba83c610bddc5
thanhtran17 <enigma15012001@gmail.com>
2023-06-28 17:05:51 +0700

    build the backend

--- web/shopify.js
+++ web/shopify.js
@@ -1,36 +1,43 @@
-import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
-import { shopifyApp } from "@shopify/shopify-app-express";
-import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
+import { BillingInterval, LATEST_API_VERSION } from '@shopify/shopify-api'
+import { shopifyApp } from '@shopify/shopify-app-express'
+import { SQLiteSessionStorage } from '@shopify/shopify-app-session-storage-sqlite'
+import { restResources } from '@shopify/shopify-api/rest/admin/2023-04'
 
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+import sqlite3 from 'sqlite3'
+import { join } from 'path'
+
+import { QRCodesDB } from './qr-codes-db.js'
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
 const billingConfig = {
-  "My Shopify One-Time Charge": {
+  'My Shopify One-Time Charge': {
     // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
     amount: 5.0,
-    currencyCode: "USD",
-    interval: BillingInterval.OneTime,
-  },
-};
+    currencyCode: 'USD',
+    interval: BillingInterval.OneTime
+  }
+}
+
+const database = new sqlite3.Database(join(process.cwd(), 'database.sqlite'))
+// Initialize SQLite DB
+QRCodesDB.db = database
+QRCodesDB.init()
 
 const shopify = shopifyApp({
   api: {
     apiVersion: LATEST_API_VERSION,
     restResources,
-    billing: undefined, // or replace with billingConfig above to enable example billing
+    billing: undefined // or replace with billingConfig above to enable example billing
   },
   auth: {
-    path: "/api/auth",
-    callbackPath: "/api/auth/callback",
+    path: '/api/auth',
+    callbackPath: '/api/auth/callback'
   },
   webhooks: {
-    path: "/api/webhooks",
+    path: '/api/webhooks'
   },
-  // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
-});
+  sessionStorage: new SQLiteSessionStorage(database)
+})
 
-export default shopify;
+export default shopify
