3f978d5c0eefc1a1375de59391fd8f99dff370b4
thanhtran17 <enigma15012001@gmail.com>
2023-06-28 16:50:38 +0700

    build the fe part - qr code

---
+++ web/frontend/components/index.js
@@ -0,0 +1,5 @@
+export { ProductsCard } from './ProductsCard'
+export { QRCodeIndex } from './QRCodeIndex'
+export { QRCodeForm } from './QRCodeForm'
+
+export * from './providers'
---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,94 @@
+import { useNavigate, TitleBar, Loading } from '@shopify/app-bridge-react'
+import {
+  Card,
+  EmptyState,
+  Layout,
+  Page,
+  SkeletonBodyText
+} from '@shopify/polaris'
+import { QRCodeIndex } from '../components'
+
+export default function HomePage() {
+  const navigate = useNavigate()
+
+  const isLoading = false
+  const isRefetching = false
+  const QRCodes = [
+    {
+      createdAt: '2022-06-13',
+      destination: 'checkout',
+      title: 'My first QR code',
+      id: 1,
+      discountCode: 'SUMMERDISCOUNT',
+      product: {
+        title: 'Faded t-shirt'
+      }
+    },
+    {
+      createdAt: '2022-06-13',
+      destination: 'product',
+      title: 'My second QR code',
+      id: 2,
+      discountCode: 'WINTERDISCOUNT',
+      product: {
+        title: 'Cozy parka'
+      }
+    },
+    {
+      createdAt: '2022-06-13',
+      destination: 'product',
+      title: 'QR code for deleted product',
+      id: 3,
+      product: {
+        title: 'Deleted product'
+      }
+    }
+  ]
+  const qrCodesMarkup = QRCodes?.length ? (
+    <QRCodeIndex QRCodes={QRCodes} loading={isRefetching} />
+  ) : null
+
+  const loadingMarkup = isLoading ? (
+    <Card sectioned>
+      <Loading />
+      <SkeletonBodyText />
+    </Card>
+  ) : null
+
+  const emptyStateMarkup =
+    !isLoading && !QRCodes?.length ? (
+      <Card sectioned>
+        <EmptyState
+          heading="Create unique QR codes for your product"
+          action={{
+            content: 'Create QR code',
+            onAction: () => navigate('/qrcodes/new')
+          }}
+          image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+        >
+          <p>
+            Allow customers to scan codes and buy products using their phones.
+          </p>
+        </EmptyState>
+      </Card>
+    ) : null
+
+  return (
+    <Page fullWidth={!!qrCodesMarkup}>
+      <TitleBar
+        title="QR codes"
+        primaryAction={{
+          content: 'Create QR code',
+          onAction: () => navigate('/qrcodes/new')
+        }}
+      />
+      <Layout>
+        <Layout.Section>
+          {loadingMarkup}
+          {qrCodesMarkup}
+          {emptyStateMarkup}
+        </Layout.Section>
+      </Layout>
+    </Page>
+  )
+}
